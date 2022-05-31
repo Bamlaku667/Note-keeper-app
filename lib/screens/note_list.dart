87f9d0fafd7091c989e13c044a919b6a4cc89d34
Bamlaku <bamlakhiruya@gmail.com>
2022-05-31 08:49:18 +0300

    routing to a note_details page

--- lib/screens/note_list.dart
+++ lib/screens/note_list.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:list_view/screens/note_details.dart';
 
 class NoteList extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return NoteListState();
   }
 }
 
-
 class NoteListState extends State<NoteList> {
-
   int count = 10;
-
 
   @override
   Widget build(BuildContext context) {
@@ -21,44 +17,43 @@ class NoteListState extends State<NoteList> {
         appBar: AppBar(
           title: Text("Note"),
         ),
-
         body: getNoteListView(),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            debugPrint("FAB pressed");
-          },
-          child: Icon(Icons.add)
-        )
+            onPressed: () {
+              debugPrint("FAB pressed");
+              NavigateToNoteDetails();
+            },
+            child: Icon(Icons.add)));
+  }
+
+  void NavigateToNoteDetails() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => NoteDetails(),
+      ),
     );
   }
 
-
-  ListView getNoteListView() {
-    
-
-     
+  Widget getNoteListView() {
     return ListView.builder(
-      itemCount: count,
-      itemBuilder: (BuildContext context, int index)  {
-        return Card(
-          color: Colors.white,
-          child: ListTile(
-            
-            leading: CircleAvatar(
-              backgroundColor: Colors.yellow,
-              child: Icon(Icons.keyboard_arrow_right),
+        itemCount: count,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            color: Colors.white,
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.yellow,
+                child: Icon(Icons.keyboard_arrow_right),
+              ),
+              title: Text("Dummy title",
+                  style: Theme.of(context).textTheme.headline6),
+              subtitle: Text("Another title"),
+              onTap: () {
+                debugPrint("item $index tapped");
+              },
             ),
-            title: Text("Dummy title", style: Theme.of(context).textTheme.headline6),
-            subtitle: Text("Another title"),
-            onTap: () {
-              debugPrint("item $index tapped");
-            },
-          ),
-
-          
-        );
-      });
+          );
+        });
   }
-} 
-
-
+}
